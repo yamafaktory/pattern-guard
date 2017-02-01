14534f9f058372d6deecc4e01ed9e1d76591f40a
Davy Duperron <yamafaktory@gmail.com>
2017-02-02 00:28:22 +0100

    First commit :rocket::tada:.

---
+++ __tests__/main.spec.js
@@ -0,0 +1,68 @@
+test('numbers', () => {
+  const Guards = require('../')
+  const [a, b, c] = [1, 3, 7]
+  const guards = new Guards()
+
+  expect(
+    guards.equal({ a, b, c })`
+      | a > b = 1337
+      | b > c = 999
+      | c > a = 42
+    `
+  ).toBe(42)
+})
+
+test('strings', () => {
+  const Guards = require('../')
+  const [a, b, c] = [1, 3, 7]
+  const guards = new Guards()
+
+  expect(
+    guards.equal({ a, b, c })`
+      | a > b = 'nope'
+      | b > c = 'nope'
+      | c > a = 'yep'
+    `
+  ).toBe('yep')
+})
+
+test('inline', () => {
+  const Guards = require('../')
+  const [a, b, c] = [1, 1, 7]
+  const guards = new Guards()
+
+  expect(
+    guards.equal({ a, b, c })`a < b = 'yep' | b > c = 'nope' | c > a = 'maybe'`
+  ).toBe('maybe')
+})
+
+test('complex comparison operators', () => {
+  const Guards = require('../')
+  const [a, b, c] = [1, 11, 11]
+  const guards = new Guards()
+
+  expect(
+    guards.equal({ a, b, c })`
+      | a === b = 1
+      | a != c = 2
+      | b >= c = 3
+      | c <= a = 4
+      | c !== a = 5
+      | c != a = 6
+    `
+  ).toBe(2)
+})
+
+test('logical operators', () => {
+  const Guards = require('../')
+  const [a, b, c] = [true, false, true]
+  const guards = new Guards()
+
+  expect(
+    guards.equal({ a, b, c })`
+      | a || b = 1
+      | b && c = 2
+      | !a = 3
+    `
+  ).toBe(1)
+})
---
+++ index.js
@@ -0,0 +1,72 @@
+const vm = require('vm')
+
+module.exports = class Guards {
+  constructor () {
+    this.guardRegex = /[^=<>!']=[^=]/
+
+    this.templateRegex = /[^\|]\|[^\|]/
+
+    this.optionsVM = {
+      displayErrors: true,
+      filename: 'guards'
+    }
+  }
+
+  getStack () {
+    const origin = Error.prepareStackTrace
+    const error = new Error
+
+    Error.prepareStackTrace = (_, stack) => stack
+
+    Error.captureStackTrace(error, this.getStack)
+
+    const stack = error.stack
+
+    Error.prepareStackTrace = origin
+
+    // V8 stack traces.
+    return stack
+  }
+
+  equal (constants) {
+    return template => {
+      const guards = this.parse(template.raw[0])
+      const lineOffset = this.getStack()[1].getLineNumber()
+
+      // First truthy guard is returned, like in Haskell.
+      return guards[(
+        guards.map(
+          g => this.runInVM(g.eval, constants, lineOffset)
+        )
+      ).findIndex(a => a === true)].result
+    }
+  }
+
+  error (e) {
+    console.error(e)
+
+    process.exit(1)
+  }
+
+  parse (template) {
+    // Inline guards need filtering.
+    return template
+      .split(this.templateRegex)
+      .filter(g => g.trim() !== '')
+      .map((g, i) => {
+        // Remove break line and extract the guard.
+        const parts = g.trim().split(this.guardRegex)
+
+        return {
+          eval: parts[0],
+          result: JSON.parse(`${parts[1].trim().replace(/'/g, '"')}`)
+        }
+      })
+  }
+
+  runInVM (code, sandbox, lineOffset) {
+    const options = Object.assign({}, this.optionsVM, { lineOffset: lineOffset })
+
+    return vm.runInNewContext(code, sandbox, options)
+  }
+}
